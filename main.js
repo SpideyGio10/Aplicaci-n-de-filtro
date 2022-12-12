747358add9fcf93f166113bdfcabb978dffc459a
SpideyGio10 <104469485+SpideyGio10@users.noreply.github.com>
2022-12-12 17:40:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(350,350);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    ave('imagen_de_mostacho.png');
+}
